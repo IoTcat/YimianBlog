86e427781af8c357fa967051268bd866ba35e249
hhCandy <i@hhcandy.me>
2019-02-20 21:39:21 +0800

    add pio

--- blog/usr/themes/handsome/component/footer.php
+++ blog/usr/themes/handsome/component/footer.php
@@ -7,7 +7,7 @@
       <?php $this->options->BottomInfo(); ?>
       Powered by <a target="blank" href="http://www.typecho.org">Typecho</a>&nbsp;|&nbsp;Theme by <a target="blank" href="https://www.ihewro.com">handsome</a>&nbsp;|&nbsp;Developed by <a target="blank" href="https://www.yimian.xyz">hhCandy</a>
       </span>
-		Copyright&nbsp;&copy;<?php echo date("Y");?>&nbsp;<a href="https://www.yimian.xyz">Yimian</a>&nbsp;|&nbsp;<a href="http://www.miitbeian.gov.cn/" target="_blank">鲁ICP备18022823号-1</a></a><?php $this->options->BottomleftInfo(); ?>
+		&copy;&nbsp;2018-<?php echo date("Y");?>&nbsp;Copyright&nbsp;<a href="https://www.yimian.xyz">Yimian</a>&nbsp;|&nbsp;<a href="http://www.miitbeian.gov.cn/" target="_blank">鲁ICP备18022823号-1</a></a><?php $this->options->BottomleftInfo(); ?>
     </div>
       <!--可以去除主题版权信息，最好保留版权信息或者添加主题信息到友链，谢谢你的理解-->
       <?php if (@in_array('showSettingsButton',$this->options->featuresetup)): ?>
